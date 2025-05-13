6b04ebcef3a3c9db515991613058ec003ee14d33
Aluno IFC <aluno@lab32-pc01.sombrio.ifc.edu.br>
2025-05-13 14:46:35 -0300

    produto adicionado

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var clientesRouter = require('./routes/clientes');
 var listaRouter = require('./routes/lista');
+var produtoRouter = require('./routes/produto');
+
 
 
 var app = express();
@@ -26,6 +28,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/clientes', clientesRouter);
 app.use('/lista', listaRouter);
+app.use('/produto', produtoRouter);
 
 
 // catch 404 and forward to error handler
--- routes/produto.js
+++ routes/produto.js
@@ -4,20 +4,20 @@ var router = express.Router();
 // Rota principal para renderizar nossa lista
 router.get('/', (req, res) => {
     // 3. Dados dinâmicos (nosso vetor de elementos)
-    const meusItens = [
-      { id: 1, nome: 'Item Alfa', descricao: 'Descrição detalhada do Item Alfa.' },
-      { id: 2, nome: 'Item Beta', descricao: 'O segundo item da nossa lista dinâmica.' },
-      { id: 3, nome: 'Item Gama', descricao: 'Terceiro e último item exemplo.' },
-      { id: 4, nome: 'Item Delta', descricao: 'Mais um item para testar.' }
+    const meusProdutos = [
+      { id: 1, nome: 'chocolate', preço: '5' },
+      { id: 2, nome: 'batata frita', preço: '18' },
+      { id: 3, nome: 'hamburguer', preço: '38' },
+      { id: 4, nome: 'pizza', preço: '45' }
     ];
   
     // 4. Renderizar a view 'lista.ejs' passando os dados
     // O segundo argumento de res.render() é um objeto.
     // As chaves desse objeto (ex: 'titulo', 'itensParaView') se tornam
     // variáveis disponíveis dentro do template EJS.
-    res.render('lista', {
-      titulo: 'Minha Lista Dinâmica',
-      itensParaView: meusItens // Passando o array para a view
+    res.render('usuarios', {
+      titulo: 'Meus produtos',
+      produtosParaView: meusProdutos // Passando o array para a view
     });
   });
 
